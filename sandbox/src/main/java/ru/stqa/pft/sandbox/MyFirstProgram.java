728374b08d7864a8d364bd861b6e4ff924d86da3
aibek17 <aibek.kabi1717@gmail.com>
2021-01-10 19:05:36 +0600

    добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,33 @@
 package ru.stqa.pft.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, "+"World");
+		hello("World");
+		hello("user");
+		hello("KZ");
+
+		double l = 5;
+		System.out.println("Pl kv " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("pl pryamoug " + a + " i " +b+ " = " + area(a,b));
+
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, "+ somebody + "!");
+	}
+
+	public static double area (double len) {
+		return len*len;
+	}
+
+	public static double area (double a, double b) {
+		return a * b;
 	}
 
 }
